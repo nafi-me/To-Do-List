127abae4d3cc953eba534c412a4cd83a2b614cd9
MD SHAHRIAR RASHID <143961339+nafi-me@users.noreply.github.com>
2025-11-13 04:30:16 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    tasks = Task.query.order_by(Task.created_at.desc()).all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form['title']
+    db.session.add(Task(title=title))
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/done/<int:id>')
+def done(id):
+    task = Task.query.get_or_404(id)
+    task.done = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
